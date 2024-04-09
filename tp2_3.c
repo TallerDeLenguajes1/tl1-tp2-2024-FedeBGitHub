51a3d86a1c2c11430f684086abaec2e5bc013322
FedeBGitHub <fedebarrionuevo76@gmail.com>
2024-04-09 13:34:32 -0300

    punto 3

---
+++ tp2_3.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+#define M 7
+int main()
+{
+    int i, j, aux;
+    int mt[N][M];
+    int *p;
+    p = &(mt[0][0]);
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            *(p + (i * M + j)) = 1 + rand() % 100;
+            printf(" [%d] ", *(p + (i * M + j)));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
